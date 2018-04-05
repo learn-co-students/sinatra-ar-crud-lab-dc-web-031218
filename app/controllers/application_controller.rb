8bb91db3b2c0f1999865b90fc500ab6354887cce
github username <github email address>
2018-04-05 09:47:08 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 
 require_relative '../../config/environment'
+require_relative '../models/post.rb'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +11,73 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @posts = Post.all 
 
+  	erb :index
   end
+  
+  get '/posts/new' do 
+  	erb :new
+  end
+  
+  get '/posts' do
+	 @posts = Post.all
+	 
+   erb :index
+  end
+
+  get '/index/' do 
+    @posts = Post.all 
+
+  	erb :index
+  end
+
+  get '/show/' do 
+  	@posts = Post.all 
+  	erb :show
+  end
+
+  post '/posts' do 
+  	@post = Post.create(name: params[:name],content:params[:content])
+  	@posts = Post.all 
+  	erb :index
+  end
+
+  get '/posts/:id' do 
+  	@post = Post.find(params[:id])
+  	if @post != nil
+  		erb :show
+  	else
+  		erb :index
+  	end
+  end
+  
+  patch '/posts/:id' do
+      @post = Post.find(params[:id])
+      @post.name = params[:name]
+      @post.content = params[:content]
+     @post.save
+
+      redirect_to "/posts/:id"
+  end
+
+  post '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+
+    erb :delete
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do 
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    erb :index
+  end
+
 end
